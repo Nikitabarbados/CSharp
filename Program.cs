f90844db1c27e02ab03a3c8ce54d5da80ff5437b
Nikita <nikita.gaber2703@gmail.com>
2025-10-14 10:28:57 +0300

    DZ
    DZ

--- Program.cs
+++ Program.cs
@@ -1,31 +1,82 @@
-﻿namespace Console_C_OOP1 // логичная область видимости, на самом деле его можно и убрать 
+﻿namespace sharp
 {
-    internal class Program // интернал говорит о том что этот тип данных программ будет доступно только на линии этого экзешника 
+    internal class Program
     {
-        // мейн будет находиться всегда в каком нибудь классе, как точка входа 
-        static void Main(string[] args) // string[] args - масив аргументов которые можно передать системе
-            // точка входа должна быть обезательно статична static
-            // если не будет статика то нашей виртуальной машине пришлось сначало создать обект с типом данных програм а потом через
-            // ссылку на этот обект вызывать функцию мейн
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!\n"); // cout - нету, все теперь на классах,
-            // console . и writeline - то есть что показать в консоли, напечать линию текста итд
-            // можно написать cw а после нажать TAB и моментально напишется Console.WriteLine();.
-            // Коротко называется сниппет, их очень много!
-            Console.WriteLine("My Name is Nikita!\n");
-            // Сам по себе WriteLine как и выводит текст так и делает переход \n!
 
-            int x = 5;
-            int y = 15;
-            double d = 3.14;
-            string s = "Simple String!";
-            string name = "Nikita!";
+            // 1
+            Console.WriteLine("1. Середнє арифметичне трьох чисел");
+            double a, b, c;
+            Console.Write("Введіть a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введіть b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Введіть c: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Середнє = " + (a + b + c) / 3);
+            Console.WriteLine();
 
-            int result = x + y;
+            // 3
+            Console.WriteLine("3. Ступінь числа");
+            double num, pow;
+            Console.Write("Число: ");
+            num = double.Parse(Console.ReadLine());
+            Console.Write("Ступінь: ");
+            pow = double.Parse(Console.ReadLine());
+            Console.WriteLine("Результат = " + (num, pow));
+            Console.WriteLine();
 
-            Console.WriteLine(result);
-            Console.WriteLine(y / d);
+            // 4
+            Console.WriteLine("4. Площа і довжина кола");
+            const double pi = 3.14159;
+            double r;
+            Console.Write("Радіус: ");
+            r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Площа = " + (pi * r * r));
+            Console.WriteLine("Довжина = " + (2 * pi * r));
+            Console.WriteLine();
 
+            // 5
+            Console.WriteLine("5. Гривні в долари і євро");
+            double uah;
+            Console.Write("Гривні: ");
+            uah = double.Parse(Console.ReadLine());
+            Console.WriteLine("Долари = " + (uah / 41));
+            Console.WriteLine("Євро = " + (uah / 44));
+            Console.WriteLine();
+
+            // 6
+            Console.WriteLine("6. Кілометри в милі");
+            double km;
+            Console.Write("Кілометри: ");
+            km = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сухопутні милі = " + (km * 0.621371));
+            Console.WriteLine("Морські милі = " + (km / 1.852));
+            Console.WriteLine();
+
+            // 7
+            Console.WriteLine("7. Відсоток P від числа N");
+            double N, P;
+            Console.Write("N: ");
+            N = double.Parse(Console.ReadLine());
+            Console.Write("P: ");
+            P = double.Parse(Console.ReadLine());
+            Console.WriteLine("Результат = " + (N * P / 100));
+            Console.WriteLine();
+
+            // 8
+            Console.WriteLine("8. Температура");
+            double cels, fahr;
+            Console.Write("Цельсій: ");
+            cels = double.Parse(Console.ReadLine());
+            fahr = (cels * 9 / 5) + 32;
+            Console.WriteLine("Фаренгейт = " + fahr);
+
+            Console.Write("Фаренгейт: ");
+            fahr = double.Parse(Console.ReadLine());
+            cels = (fahr - 32) * 5 / 9;
+            Console.WriteLine("Цельсій = " + cels);
         }
     }
 }
