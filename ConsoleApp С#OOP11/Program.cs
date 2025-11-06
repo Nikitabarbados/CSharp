b437aac56c7f2c39f7f3dad9cf62a1fd53b8a184
Nikita <nikita.gaber2703@gmail.com>
2025-11-06 19:39:11 +0200

    dz
    dz

---
+++ ConsoleApp С#OOP11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_С_OOP11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
