689d1ff0a713927474de26d3ec24e8e8abd797fb
ZemNyux <nikita.gaber2703@gmail.com>
2026-02-15 17:24:49 +0200

    db
    db

--- Program.cs
+++ Program.cs
@@ -1,139 +1,51 @@
 ﻿using System;
+using System.Linq;
 
-class Student
+class Program
 {
-    private string lastName;
-    private string firstName;
-    private string middleName;
-    private DateTime birthDate;
-    private string address;
-    private string phone;
-
-    private int[] credits;
-    private int[] courseWorks;
-    private int[] exams;
-
-    public Student()
+    static void Main()
     {
-        lastName = "";
-        firstName = "";
-        middleName = "";
-        birthDate = DateTime.MinValue;
-        address = "";
-        phone = "";
-        credits = new int[3];
-        courseWorks = new int[2];
-        exams = new int[3];
-    }
-
-    public Student(string ln, string fn, string mn, DateTime bd, string addr, string ph)
-    {
-        lastName = ln;
-        firstName = fn;
-        middleName = mn;
-        birthDate = bd;
-        address = addr;
-        phone = ph;
-        credits = new int[3];
-        courseWorks = new int[2];
-        exams = new int[3];
-    }
-    public string LastName { 
-
-        get { 
-            return lastName; 
-        } 
-        set { 
-            lastName = value; 
-        } 
-    }
-    public string FirstName {
-
-        get {
-            return firstName;
-        }
-        set {
-            firstName = value; 
-        }
-    }
-    public string MiddleName {
-
-        get { 
-            return middleName; 
-        } 
-        set 
+        using (var db = new AppDbContext())
         {
-            middleName = value;
+            db.Database.EnsureCreated();
+
+            Console.WriteLine("Enter login:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6 || !password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Password must be at least 6 characters and contain at least one digit.");
+                return;
+            }
+
+            var user = db.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user == null)
+            {
+                var newUser = new User
+                {
+                    Username = username,
+                    Password = PasswordHelper.HashPassword(password),
+                    CreatedAt = DateTime.Now
+                };
+
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                Console.WriteLine("User registered successfully!");
+            }
+            else
+            {
+                string hashedInput = PasswordHelper.HashPassword(password);
+
+                if (user.Password == hashedInput)
+                    Console.WriteLine("Login successful!");
+                else
+                    Console.WriteLine("Wrong password!");
+            }
         }
-    }
-    public string Address {
-
-        get { 
-            return address; 
-        } 
-        set { 
-            address = value; 
-        }
-    }
-    public string Phone { 
-
-        get { 
-            return phone; 
-        }
-        set { 
-            phone = value; 
-        } 
-    }
-    public DateTime BirthDate { 
-        get {
-            return birthDate; 
-        } 
-        set {
-            birthDate = value;
-        } 
-    }
-
-    public void SetCredits(int a, int b, int c)
-    {
-        credits[0] = a; 
-        credits[1] = b;
-        credits[2] = c;
-    }
-
-    public void SetCourseWorks(int a, int b)
-    {
-        courseWorks[0] = a; 
-        courseWorks[1] = b;
-    }
-
-    public void SetExams(int a, int b, int c)
-    {
-        exams[0] = a; 
-        exams[1] = b;
-        exams[2] = c;
-    }
-    public double Average()
-    {
-        int sum = 0;
-        int count = credits.Length + courseWorks.Length + exams.Length;
-
-        for (int i = 0; i < credits.Length; i++)
-            sum += credits[i];
-        for (int i = 0; i < courseWorks.Length; i++)
-            sum += courseWorks[i];
-        for (int i = 0; i < exams.Length; i++) 
-            sum += exams[i];
-
-        return (double)sum / count;
-    }
-
-    public void Show()
-    {
-        Console.WriteLine("Студент: " + lastName + " " + firstName + " " + middleName);
-        Console.WriteLine("Дата народження: " + birthDate.ToShortDateString());
-        Console.WriteLine("Адреса: " + address);
-        Console.WriteLine("Телефон: " + phone);
-        Console.WriteLine("Середній бал: " + Average());
-        Console.WriteLine();
     }
 }
